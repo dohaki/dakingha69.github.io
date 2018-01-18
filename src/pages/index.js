b451d6e15fff2abf5ff9061762c46c34b0387665
Dong-Ha Kim <dongha.kim210@gmail.com>
2018-01-18 23:18:08 +0100

    Change index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,48 @@
 import React from 'react'
 import Link from 'gatsby-link'
 
+import HorizontalNav from '../components/HorizontalNav'
+import VerticalNav from '../components/VerticalNav'
+
 const IndexPage = () => (
-  <div>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
+  <div
+    style={{
+      height: '100%',
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'space-between',
+      textAlign: 'center'
+    }}
+  >
+    <VerticalNav
+      link='/page-2/'
+      name='ABOUT'
+    />
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+      }}
+    >
+      <HorizontalNav
+        direction='left'
+        link='/page-2/'
+        name='CV'
+      />
+      <div>
+        <h1>DH.Kim</h1>
+      </div>
+      <HorizontalNav
+        direction='right'
+        link='/page-2/'
+        name='PROJECTS'
+      />
+    </div>
+    <VerticalNav
+      link='/page-2/'
+      name='CONTACT'
+    />
   </div>
 )
 
