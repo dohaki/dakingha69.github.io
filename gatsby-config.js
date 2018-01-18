a4a770144d710ae571f05eb67d4b46768e47898c
Dong-Ha Kim <dongha.kim210@gmail.com>
2018-01-18 23:17:41 +0100

    Change title

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Default Starter',
+    title: 'DH.Kim',
   },
   plugins: ['gatsby-plugin-react-helmet'],
 };
--- src/layouts/index.js
+++ src/layouts/index.js
@@ -2,25 +2,24 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 
-import Header from '../components/Header'
 import './index.css'
 
 const TemplateWrapper = ({ children }) => (
-  <div>
+  <div
+    style={{
+      height: '100%'
+    }}
+  >
     <Helmet
-      title="Gatsby Default Starter"
+      title="DH.Kim"
       meta={[
         { name: 'description', content: 'Sample' },
         { name: 'keywords', content: 'sample, something' },
       ]}
     />
-    <Header />
     <div
       style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '0px 1.0875rem 1.45rem',
-        paddingTop: 0,
+        height: '100%'
       }}
     >
       {children()}
