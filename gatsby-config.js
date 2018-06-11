af91d3a38e3789b75bddbc41d5e029c7504f773f
Dong-Ha Kim <dongha.kim210@gmail.com>
2018-06-11 10:34:46 +0200

    Updates

--- gatsby-config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  siteMetadata: {
-    title: 'DH.Kim',
-    siteUrl: `https://www.dongha.kim`,
-    description: `Welcome`,
-  },
-  plugins: ['gatsby-plugin-react-helmet'],
-};
