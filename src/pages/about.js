74d104c9d93f6d5edd407bb3ef50d12813db7c50
dakingha69 <dongha.kim210@gmail.com>
2018-12-02 16:56:58 +0100

    Add content to about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,18 +1,26 @@
 import React from 'react';
-import { Box, Heading } from 'grommet';
+import { Anchor, Paragraph } from 'grommet';
 
-import Layout from '../components/Layout';
-import TopNav from '../components/TopNav';
+import PageTemplate from '../components/PageTemplate';
 
 const AboutPage = () => (
-  <Layout>
-    <Box>
-      <TopNav />
-      <Heading>
-        Hello world!
-      </Heading>
-    </Box>
-  </Layout>
+  <PageTemplate title="Hello world!">
+    <Paragraph alignSelf="center">
+      Welcome to my website. My name is Dong-Ha Kim and I am
+      a Fullstack Javascript Developer, Blockchain Entusiast and
+      M. Sc. Student at the TU Berlin.
+    </Paragraph>
+    <Paragraph alignSelf="center">
+      This site is unfortunately still WIP. You will find different
+      projects and blog posts of mine in the near future here. Until
+      then check out my&nbsp;
+      <Anchor label="github" href="https://github.com/dakingha69" />
+      .
+    </Paragraph>
+    <Paragraph alignSelf="center">
+      Some more things I like are FOOD 😋, TECHNO 🕺, GAMES 🎮 and CATS 🐱.
+    </Paragraph>
+  </PageTemplate>
 );
 
 export default AboutPage;
