a7d01adb099df938d9cf82d79be4cad01b15f7e5
dakingha69 <dongha.kim210@gmail.com>
2018-12-15 16:07:01 +0100

    Add SkillMeter component

---
+++ src/components/SkillMeter.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Box, Meter, Text } from 'grommet';
+
+const SkillMeter = ({ name, level }) => {
+  return (
+    <Box>
+      <Text>
+        {name}
+      </Text>
+      <Meter
+      type="bar"
+        values={[{ value: level }]}
+        margin={{ top: "small" }}
+        background="light-2"
+      />
+    </Box>
+  );
+}
+ 
+export default SkillMeter;
