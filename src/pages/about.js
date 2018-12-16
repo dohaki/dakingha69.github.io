c8810d2edba7670f8e650868699a0eb3a2e2b04f
dakingha69 <dongha.kim210@gmail.com>
2018-12-16 22:35:09 +0100

    Add SkillBox to about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,29 +1,49 @@
 import React from 'react';
-import { Anchor, Paragraph } from 'grommet';
+import { Anchor, Box, Heading, Paragraph } from 'grommet';
 
 import PageTemplate from '../components/PageTemplate';
+import SkillBox from '../components/SkillBox';
+
+import { CATEGORIES, SKILLS } from '../constants';
 
 const AboutPage = () => (
   <PageTemplate title="Hello world!">
-    <Paragraph alignSelf="center">
-      Welcome to my website. My name is Dong-Ha Kim and I am
-      a Fullstack Javascript Developer, Blockchain Enthusiast and
-      M. Sc. Student at the TU Berlin.
-    </Paragraph>
-    <Paragraph alignSelf="center">
-      This site is unfortunately still WIP. You will find different
-      projects and blog posts of mine in the near future here. Until
-      then check out my&nbsp;
-      <Anchor label="github" href="https://github.com/dakingha69" />
-      .
-    </Paragraph>
-    <Paragraph alignSelf="center">
-      Some more things I like are
-      FOOD <span role="img" aria-label="food">😋</span>,
-      TECHNO <span role="img" aria-label="dance">🕺</span>,
-      GAMES <span role="img" aria-label="game">🎮</span>
-      and CATS <span role="img" aria-label="cats">🐱</span>.
-    </Paragraph>
+    <Box align="center">
+      <Paragraph>
+        Welcome to my website. My name is Dong-Ha Kim and I am
+        a Fullstack Javascript Developer, Blockchain Enthusiast and
+        M. Sc. Student at the TU Berlin.
+      </Paragraph>
+      <Paragraph>
+        This site is unfortunately still WIP. You will find different
+        projects and blog posts of mine in the near future here. Until
+        then check out my&nbsp;
+        <Anchor label="github" href="https://github.com/dakingha69" />
+        .
+      </Paragraph>
+      <Heading>
+        Skills
+      </Heading>
+      <Paragraph>
+        Below you can find a list of my skills with an estimated level on how
+        proficient I see myself.
+      </Paragraph>
+      {CATEGORIES && CATEGORIES.map(category => (
+        <SkillBox
+          key={category}
+          category={category}
+          skills={SKILLS.filter(skill => skill.category === category)}
+        />
+      ))}
+      <SkillBox></SkillBox>
+      <Paragraph>
+        Some more things I like are
+        FOOD <span role="img" aria-label="food">😋</span>,
+        TECHNO <span role="img" aria-label="dance">🕺</span>,
+        GAMES <span role="img" aria-label="game">🎮</span>
+        and CATS <span role="img" aria-label="cats">🐱</span>.
+      </Paragraph>
+    </Box>
   </PageTemplate>
 );
 
