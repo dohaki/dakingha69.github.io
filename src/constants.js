4dbac6e469000fb9434903e0280e125c2d8580f5
dakingha69 <dongha.kim210@gmail.com>
2018-12-16 22:34:52 +0100

    Add SkillBox component

--- src/constants.js
+++ src/constants.js
@@ -16,3 +16,62 @@ export const PAGES = [
     name: 'PROJECTS'
   }
 ];
+
+export const CATEGORIES = [
+  'Javascript',
+  'Ethereum',
+  'Tools',
+]
+
+export const SKILLS = [
+  {
+    category: 'Javascript',
+    name: 'ES6+',
+    level: 85
+  },
+  {
+    category: 'Javascript',
+    name: 'TypeScript',
+    level: 85
+  },
+  {
+    category: 'Javascript',
+    name: 'React / React Native',
+    level: 80
+  },
+  {
+    category: 'Javascript',
+    name: 'Node.js',
+    level: 70
+  },
+  {
+    category: 'Tools',
+    name: 'Git',
+    level: 75
+  },
+  {
+    category: 'Tools',
+    name: 'Travis CI',
+    level: 60
+  },
+  {
+    category: 'Tools',
+    name: 'Docker',
+    level: 60
+  },
+  {
+    category: 'Ethereum',
+    name: 'Solidity',
+    level: 70
+  },
+  {
+    category: 'Ethereum',
+    name: 'Truffle Framework',
+    level: 80
+  },
+  {
+    category: 'Ethereum',
+    name: 'web3.js',
+    level: 85
+  },
+];
